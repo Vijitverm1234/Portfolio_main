9e7047ec7ca9bda4e418061a7339a714f0597399
Vijitverm1234 <vijitverma78@gmail.com>
2026-01-13 23:25:49 +0530

    feat(client):working on its hero component

--- my-react-app/src/components/Header/Header.jsx
+++ my-react-app/src/components/Header/Header.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import vijitimg from "../../assets/vijitimage.jpg";
+import pattern from "../../assets/background.png"
 const Header = () => {
   return (
-    <div>
-      Hello
+    <div className="head-sec">
+      <img src={pattern} alt="decor" class="head-decor" />
+      <div className="head-left">
+        <div className="left-container">
+          <div className="left-back"></div>
+          <div className="left-img">
+            <img src={vijitimg} alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="head-right"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
